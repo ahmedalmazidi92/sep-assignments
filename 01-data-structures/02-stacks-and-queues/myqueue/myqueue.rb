9fa75451e976d166fd4a448d1adaac8f60992e8f
Ahmed Almazidi <a.almazidi92@gmail.com>
2017-11-13 22:51:12 +0300

    Assignment 2 complete

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -8,11 +8,20 @@ class MyQueue
   end
 
   def enqueue(element)
+    @head = element if empty?
+    last_index = @queue.length
+    @queue[last_index] = element
+    @tail = element
   end
 
   def dequeue
+    item = @queue.delete_at(0)
+    @head = @queue[0]
+    @tail = @queue.last
+    item
   end
 
   def empty?
+    @head.nil?
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,18 @@ class MyStack
   end
 
   def push(item)
+    @top = item
+    top_of_stack = @stack.length
+    @stack[top_of_stack] = item
   end
 
   def pop
+    item = @stack.delete_at(@stack.length - 1)
+    @top = @stack.last
+    item
   end
 
   def empty?
+    @top.nil?
   end
 end
